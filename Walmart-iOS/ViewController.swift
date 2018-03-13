cbe82004ae583a6653387494e63cdd8bc41a511b
Tri Ngo <ngominhtrint@gmail.com>
2018-03-13 11:19:17 +0700

    Initial project

---
+++ Walmart-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Walmart-iOS
+//
+//  Created by TriNgo on 3/13/18.
+//  Copyright © 2018 TriNgo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
